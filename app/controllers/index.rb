73af5deb1c70f9aa0a39f32b0d6f76b0f7e05702
Apprentice <apprentice@devbootcamp.com>
2013-04-08 16:21:28 -0700

    routing for new game from index page complete

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -3,11 +3,32 @@ get '/' do
   erb :index
 end
 
-post '/game' do
-  params.inspect
-  params.each_value do |name|
+post '/games/new' do
+  params.each_with_index do |(player,name), i|
     Player.find_or_create_by_username(name)
+    if i == 0
+      session[:player1_name] = name
+    elsif i == 1
+      session[:player2_name] = name
+    end
   end
 
-  "hi"
+  player1 = Player.find_by_username(session[:player1_name])
+  player2 = Player.find_by_username(session[:player2_name])
+
+  @game = Game.create(player1_id: player1.id, player2_id: player2.id)
+
+  # erb :game
+  redirect "/games/#{@game.id}"
 end
+
+get '/games/:id' do
+  @game = Game.find(params[:id])
+  # raise params.inspect
+  # Winner
+  # Length of time
+  # Status
+  erb :game
+
+end
+
