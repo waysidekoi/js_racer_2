ff028352dfb4cbf14b34dd04c9eb948a8353d0e6
Apprentice <apprentice@devbootcamp.com>
2013-04-08 15:47:34 -0700

    start game with 2 new or old players

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -2,3 +2,12 @@ get '/' do
   # Look in app/views/index.erb
   erb :index
 end
+
+post '/game' do
+  params.inspect
+  params.each_value do |name|
+    Player.find_or_create_by_username(name)
+  end
+
+  "hi"
+end
--- public/js/application.js
+++ public/js/application.js
@@ -5,3 +5,51 @@ $(document).ready(function() {
 
   // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
 });
+
+
+
+
+// $(document).ready(function() {
+//   var game_over = false;
+//   var endPoint = $('.player1_strip td').size()-1
+
+//   $(document).on('keyup', function(event) {
+//     var pressed_key = String.fromCharCode(event.which);
+
+//     if (game_over){
+//       // do northing
+//     }else if (pressed_key == 'Q') {
+//       increment('1');
+//     }
+//     else if (pressed_key == 'P') {
+//       increment('2');
+//     };  
+
+//     if ($('.player2_strip .active').index() == endPoint) {
+
+//       $('.message span').replaceWith('Player 2 Finished!');
+
+//       game_over = true;
+//     }
+
+//     else if ($('.player1_strip .active').index() == endPoint) {
+//       $('.message span').replaceWith('Player 1 Finished!');
+//       game_over = true;
+
+//     };
+//   });
+
+//   $(".reset").on('click', 'button', function() {
+//     // console.log($(this).text() + "was clicked");
+//     $('.racer_table').find('.active').removeClass('active');
+//     $('.racer_table td:first-child').addClass('active');
+//     game_over = false
+
+//   });
+
+
+// });
+
+// function increment(player_num){
+//   $('.player'+player_num+'_strip td.active').removeClass('active').next().addClass('active');
+// };
